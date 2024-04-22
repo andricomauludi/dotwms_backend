506caa3f3bff68201555616ad7bbbfb79377a7ff
andricomauludi <mauludi21@gmail.com>
2024-04-22 21:13:23 +0700

    cors

--- app.js
+++ app.js
@@ -23,6 +23,26 @@ app.options('*', cors()) ;
 app.use(express.json());
 app.use(cookieParser()); //bisa mengambil value dari cookie
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://wms.dots.co.id');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(express.urlencoded({ extended: true })); //extended true akan menghilangkan object :null protoype, kalo false akan muncul si objectnya
 
 mongoose.connect(process.env.mongodb_connection);
