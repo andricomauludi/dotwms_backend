98b7279eef817da17b08721052a70521d90d83e6
andricomauludi <mauludi21@gmail.com>
2024-06-11 13:56:27 +0700

    clean code

--- app.js
+++ app.js
@@ -13,44 +13,8 @@ dotenv.config();
 const app = express();
 const PORT = 3001; //menjalankan di port 3001
 
-
-// const corsOptions = {
-//   origin: '*', // Allow requests from this origin
-//   methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE', // Allow only GET and POST requests
-//   allowedHeaders: 'Content-Type,Authorization', // Allow only specified headers  
-// };
-
-// app.use(cors());
-
-
-// app.options('*', cors()) ;
-
-
-
-
 app.use(express.json());
 app.use(cookieParser()); //bisa mengambil value dari cookie
-
-
-// // Add headers before the routes are defined
-// app.use(function (req, res, next) {
-
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', 'https://wms.dots.co.id');
-
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
 
 
 
@@ -58,7 +22,6 @@ app.use(express.urlencoded({ extended: true })); //extended true akan menghilang
 
 mongoose.connect(process.env.mongodb_connection);
 
-// const allowedOrigins = ['https://wms.dots.co.id'];
 const corsOptions = {
   origin: 'https://wms.dots.co.id',
   credentials: true,
@@ -71,17 +34,6 @@ app.use("/users", usersRoutes);
 app.use("/workspaces", workspacesRoutes);
 app.use("/dashboard", dashboardRoutes);
 
-// db.mongoose
-//     .connect(db.url,{
-//       useNewUrlParser:true,
-//       useUnifiedTopology:true //Konfigurasi basic
-//     })
-//     .then(() =>{
-//       console.log(`database connected`)
-//     }).catch((err) => {
-//       console.log(`cannot connect to the database`, err)
-//       process.exit()    //tidak akan melanjutkan proses apabila terdapateror
-//     })
 
 app.get("/", (req, res) => {
   res.send("Hello from homepage");
