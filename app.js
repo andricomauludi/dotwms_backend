99c907905cdd5063ca62964b3765fa87eb1889ff
andricomauludi <mauludi21@gmail.com>
2024-10-26 15:46:46 +0700

    change cors

--- app.js
+++ app.js
@@ -39,6 +39,13 @@ app.use(express.urlencoded({ extended: true })); //extended true akan menghilang
 
 mongoose.connect(process.env.mongodb_connection);
 
+const corsOptions = {
+  origin: "https://wms.dots.co.id",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use("/users", usersRoutes);
 app.use("/workspaces", workspacesRoutes);
 app.use("/dashboard", dashboardRoutes);
